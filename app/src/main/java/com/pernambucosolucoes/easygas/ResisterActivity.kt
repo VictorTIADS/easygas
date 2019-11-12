acbe8462033c7c3e147a7a7bdc511bd8f541c074
victortiads <baappaap123@hotmail.com>
2019-11-11 22:58:01 -0300

    SignUp has been added

---
+++ app/src/main/java/com/pernambucosolucoes/easygas/ResisterActivity.kt
@@ -0,0 +1,12 @@
+package com.pernambucosolucoes.easygas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.resister_scroll_activity)
+    }
+}
--- app/src/main/java/com/pernambucosolucoes/easygas/SplashActivity.kt
+++ app/src/main/java/com/pernambucosolucoes/easygas/SplashActivity.kt
@@ -1,12 +1,18 @@
 package com.pernambucosolucoes.easygas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        lblSignUpUserGoToForm.setOnClickListener {
+            startActivity(Intent(this,ResisterActivity::class.java))
+        }
     }
 }
