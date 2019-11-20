8a031d648c01c784cc75068669ecb3045155761e
victortiads <baappaap123@hotmail.com>
2019-11-20 10:03:35 -0300

    Toolbar is Working

--- app/src/main/java/com/pernambucosolucoes/easygas/LoginActivity.kt
+++ app/src/main/java/com/pernambucosolucoes/easygas/LoginActivity.kt
@@ -2,11 +2,25 @@ package com.pernambucosolucoes.easygas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.toolbar_left.*
+import kotlinx.android.synthetic.main.toolbartext_right.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        configureToolbar()
+    }
+
+
+    private fun configureToolbar(){
+        setSupportActionBar(toolbar_left)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar_left.navigationIcon = getDrawable(R.drawable.ic_arrow_back_black_24dp)
+        toolbar_right.title = "Login"
     }
 }
+
+
--- app/src/main/java/com/pernambucosolucoes/easygas/ResisterActivity.kt
+++ app/src/main/java/com/pernambucosolucoes/easygas/ResisterActivity.kt
@@ -2,11 +2,20 @@ package com.pernambucosolucoes.easygas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.toolbar_left.*
+import kotlinx.android.synthetic.main.toolbartext_right.*
 
 class ResisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.masterresister_layout)
+        configureToolbar()
+    }
+    private fun configureToolbar(){
+        setSupportActionBar(toolbar_left)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar_left.navigationIcon = getDrawable(R.drawable.ic_arrow_back_black_24dp)
+        toolbar_right.title = "Cadastrar"
     }
 }
